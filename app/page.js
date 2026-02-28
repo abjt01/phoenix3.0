d335dd2a6c542f3015deb338c6bf967f72ecac53
Saiprasad Rao <saiprasadrao1234@gmail.com>
2026-02-28 23:54:19 +0530

    blur effect + exciting

--- app/page.js
+++ app/page.js
@@ -5,11 +5,12 @@ import Grainient from '@/components/ui/Grainient/Grainient';
 import CountdownTimer from '@/components/CountdownTimer';
 import CountUp from '@/components/CountUp';
 import ScrollVelocity from '@/components/ScrollVelocity';
+import { TextAnimate } from '@/components/ui/text-animate';
 export default function HomePage() {
   let targetDate = new Date("2026-03-15T23:59:00+05:30");
   const now = new Date();
   const diffMs = targetDate - now;
-  const diffDays = diffMs / (1000 * 60 * 60 * 24); 
+  const diffDays = diffMs / (1000 * 60 * 60 * 24);
   return (
     <>
       {/* Hero Section */}
@@ -35,9 +36,9 @@ export default function HomePage() {
 
         {/* ScrollVelocity Decorative Top */}
         <div className="text-sl font-bold absolute top-10 md:top-30 w-full z-0 opacity-20 pointer-events-none overflow-hidden">
-          
+
           <ScrollVelocity
-            texts={[`👉🏻${diffDays.toFixed(0)} Days Until Registration Closes👈🏼` ]}
+            texts={[`👉🏻${diffDays.toFixed(0)} Days Until Registration Closes👈🏼`]}
             velocity={50}
             className="text-white"
           />
@@ -45,10 +46,14 @@ export default function HomePage() {
 
         <div className="relative z-10 text-center max-w-5xl mt-10 md:mt-32">
           <h1 className="text-5xl md:text-9xl font-bold leading-[1.1] tracking-tighter mt-0 md:mt-20 glow-text ">
-            <span className="gradient-title">PHOENIX 3.0</span> <br />  
+            <span className="gradient-title">PHOENIX 3.0</span> <br />
           </h1>
           <h1 className="text-4xl md:text-5xl font-bold leading-[1.1] tracking-tighter mt-2  glow-text ">
-            <span className="text-white opacity-80 bg-clip-text bg-gradient-to-r from-primary to-[#ff9d5c]">Rise. Rebuild. Reinvent.</span>
+            <span className="text-white opacity-80 bg-clip-text bg-gradient-to-r from-primary to-[#ff9d5c]">
+              <TextAnimate animation="blurIn" as="h1">
+                Rise. Rebuild. Reinvent.
+              </TextAnimate>
+            </span>
           </h1>
           <p className="text-base md:text-xl text-white/70 max-w-2xl mx-auto mb-6  mt-2 font-light leading-relaxed">
             Annual Celebration of Knowledge, Creativity, and Innovation.{" "}
@@ -116,7 +121,6 @@ export default function HomePage() {
             {/* Horizontal divider for mobile */}
             <div className="col-span-2 h-px w-full bg-gradient-to-r from-transparent via-white/10 to-transparent md:hidden"></div>
 
-            <div className="hidden md:block stat-divider"></div>
 
             {/* <div className="stat-item flex flex-col items-center justify-center">
               <div className="stat-value text-3xl md:text-[2.5rem] lg:text-[3.5rem]">
@@ -127,11 +131,11 @@ export default function HomePage() {
 
             <div className="hidden md:block stat-divider"></div>
 
-            <div className="stat-item flex flex-col items-center justify-center">
-              <div className="stat-value text-3xl md:text-[2.5rem] lg:text-[3.5rem]">
-                <CountUp to={200} />K+
+            <div className="col-span-2 md:col-span-1 stat-item flex flex-col items-center justify-center">
+              <div className="stat-value text-3xl md:text-[2.5rem] lg:text-[3.5rem] ">
+                 Exciting
               </div>
-              <div className="stat-label text-[0.6rem] md:text-[0.7rem]">Prize Pool</div>
+              <div className="stat-label text-[0.6rem] md:text-[0.7rem]">Cash Prizes</div>
             </div>
 
           </div>
